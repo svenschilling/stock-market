9e9f42962d776f4868da4e626596e89511fefde0
svenschilling <sven.schilling@gmail.com>
2022-09-05 11:55:05 +0200

    component test

---
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-item',
+  templateUrl: './stock-item.component.html',
+  styleUrls: ['./stock-item.component.css']
+})
+export class StockItemComponent implements OnInit {
+
+  public name!: string;
+  public code!: string;
+  public price!: number;
+  public previousPrice!: number;
+  public positiveChange!: boolean;
+  public favorite!: boolean;
+
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.name = 'test stock company';
+    this.code = 'TSC';
+    this.price = 85;
+    this.previousPrice = 80;
+    this.positiveChange = this.price >= this.previousPrice;
+    this.favorite = false;
+  }
+
+  toggleFavorite() {
+    console.log('toggle fav-state for this stock');
+    this.favorite = !this.favorite;
+  }
+}
